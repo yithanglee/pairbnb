c66d7009e9c3b3246af5f5aef4525f71e31fb15e
nextacademy <yithanglee@gmail.com>
2016-10-13 20:14:28 +0800

    solved the routing issue from initializer

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   
-  match '/help', to: 'static_pages#help', via: 'get'
+    root 'static_pages#home'
 
-  get 'users/new'
-
-  root 'static_pages#home'
-
-  # get 'static_pages/home'
-
-  get 'static_pages/help'
+    get 'static_pages/help'
+resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+resource :session, controller: "clearance/sessions", only: [:create]  
+resources :users, controller: "clearance/users", only: [:create] do
+resource :password,controller: "clearance/passwords",only: [:create, :edit, :update]
+end  
+get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+get "/sign_up" => "clearance/users#new", as: "sign_up"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
